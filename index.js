7bd545084f54ddd0dbd1e1dad3ed404081e5de67
satanikaushal <kaushalsatani@gmail.com>
2023-02-15 22:26:17 +0530

    done with the Schemas and made few endpoints

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const express = require('express');
 connectToMongo();
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello Kaushal!')
-})
+// Available routes 
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
