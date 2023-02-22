3c27dad22a9567acfd16a9ce00d1335dc7676c41
satanikaushal <kaushalsatani@gmail.com>
2023-02-22 21:46:31 +0530

    CHANGEScommit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 connectToMongo();
 const app = express()
-const port = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json());
@@ -12,6 +12,6 @@ app.use(express.json());
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
